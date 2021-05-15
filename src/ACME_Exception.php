de02ef2696f4abb582e3a0688f1d6969113e407c
Fernando Herrero <alphp@users.noreply.github.com>
2021-05-15 20:56:18 +0200

    First changes

--- src/ACME_Exception.php
+++ src/ACME_Exception.php
@@ -1,28 +1,27 @@
 <?php
-
 /*
-  MIT License
+		MIT License
 
-  Copyright (c) 2018 Stefan Körfgen
+		Copyright (c) 2018 Stefan Körfgen
 
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
+		Permission is hereby granted, free of charge, to any person obtaining a copy
+		of this software and associated documentation files (the "Software"), to deal
+		in the Software without restriction, including without limitation the rights
+		to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+		copies of the Software, and to permit persons to whom the Software is
+		furnished to do so, subject to the following conditions:
 
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
+		The above copyright notice and this permission notice shall be included in all
+		copies or substantial portions of the Software.
 
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-  SOFTWARE.
-*/
+		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+		IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+		FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+		AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+		LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+		OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+		SOFTWARE.
+	*/
 
 // https://github.com/skoerfgen/ACMECert
 
@@ -30,17 +29,45 @@ namespace skoerfgen\ACMECert;
 
 use Exception;
 
+/**
+ * ACMECert Exception
+ *
+ * @package skoerfgen\ACMECert
+ */
 class ACME_Exception extends Exception {
-	private $type,$subproblems;
-	function __construct($type,$detail,$subproblems=array()){
-		$this->type=$type;
-		$this->subproblems=$subproblems;
-		parent::__construct($detail.' ('.$type.')');
+	protected string $type;
+	protected array $subproblems;
+
+	/**
+	 * Construct the Exception
+	 *
+	 * @param string $type Type of Exception
+	 * @param string $detail Detail of Exception
+	 * @param array $subproblems Subproblems of Exception
+	 * @return void
+	 */
+	function __construct(string $type, string $detail, array $subproblems = []) {
+		$this->type = $type;
+		$this->subproblems = $subproblems;
+
+		parent::__construct(sprintf('%s (%s)', $detail, $type));
 	}
-	function getType(){
+
+	/**
+	 * Get type of Exception
+	 *
+	 * @return string
+	 */
+	function getType() {
 		return $this->type;
 	}
-	function getSubproblems(){
+
+	/**
+	 * Get subproblems of Exception
+	 *
+	 * @return array
+	 */
+	function getSubproblems() {
 		return $this->subproblems;
 	}
 }
